37b06f4a8a6009ec815f9561bbd03beba4d2681a
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2024-04-24 10:36:09 -0700

    finsihed
    finished

--- BasicGamePlayGonzalezP3/Assets/Scripts/GameManager - Copy.cs
+++ BasicGamePlayGonzalezP3/Assets/Scripts/GameManager - Copy.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private int score = 0; 
+    private int score = 0;
     private int lives = 3;
 
-
-    // Update is called once per frame
     public void AddLives(int value)
     {
         lives += value;
+
         if (lives <= 0)
-        { Debug.Log("Game Over"); 
-            lives = 0; 
+        {
+            Debug.Log("Game Over");
+            lives = 0;
         }
         Debug.Log("Lives = " + lives);
     }
     public void AddScore(int value)
-    { score += value; 
-        Debug.Log("Score = " + score); 
+    {
+        score += value;
+        Debug.Log("Score = " + score);
     }
-
 }
+
+
